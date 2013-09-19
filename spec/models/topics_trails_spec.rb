f6384419b9787d318e7098c3800aa7e071eb6376
Eduardo Maia <eduvimaia@gmail.com>
2013-09-19 13:58:40 -0300

    included new expectations into the counter_cache spec

--- spec/models/topics_trails_spec.rb
+++ spec/models/topics_trails_spec.rb
@@ -5,13 +5,19 @@ describe TopicsTrail do
     topics = [Topic.create(name: "first topic"), Topic.create(name: "second topic")]
     trail = Trail.create(name: "trail", topics: topics)
     trail.reload
-    expect(trail.topics_count).to eq 2
+    expect(trail.topics.size).to  eq 2 # fails
+    expect(trail.topics_count).to eq 2 # fails
+    expect(trail.topics.count).to eq 2 # passes
+    expect(trail.topics.count).to eql trail.topics.size # fails
   end
 
   it "works using FG'" do
     topics = [create(:topic), create(:topic)]
     trail = create(:trail, topics: topics)
     trail.reload
-    expect(trail.topics_count).to eq 2
+    expect(trail.topics.size).to  eq 2 # fails
+    expect(trail.topics_count).to eq 2 # fails
+    expect(trail.topics.count).to eq 2 # passes
+    expect(trail.topics.count).to eql trail.topics.size # fails
   end
 end
