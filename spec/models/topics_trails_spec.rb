64475f5a61455ed28170ad9947ca76c02aa49e79
Eduardo Maia <eduvimaia@gmail.com>
2013-09-19 13:52:01 -0300

    same test using AR included

--- spec/models/topics_trails_spec.rb
+++ spec/models/topics_trails_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe TopicsTrail do
-  it "works" do
+  it "works using AR" do
+    topics = [Topic.create(name: "first topic"), Topic.create(name: "second topic")]
+    trail = Trail.create(name: "trail", topics: topics)
+    trail.reload
+    expect(trail.topics_count).to eq 2
+  end
+
+  it "works using FG'" do
     topics = [create(:topic), create(:topic)]
     trail = create(:trail, topics: topics)
     trail.reload
